3215d6dd21f072327aa43b483152b68e723cad1c
Martin <martin@automata.tech>
2023-07-25 12:57:27 +0100

    fix line height

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,9 @@ struct Args {
     mode: Mode,
 }
 
-fn paint_values(img: &image::DynamicImage, cols: u32, rows: u32, invert: bool) -> Vec<char> {
+fn paint_values(img: &image::DynamicImage, cols: u32, line_height: f32, invert: bool) -> Vec<char> {
+    let ar = img.width() as f32 / img.height() as f32;
+    let rows = (cols as f32 / (ar * line_height)) as u32;
     let char_matrix = img
         .resize_exact(cols, rows, image::imageops::FilterType::Nearest)
         .to_luma8()
@@ -76,11 +78,14 @@ fn paint_values(img: &image::DynamicImage, cols: u32, rows: u32, invert: bool) -
     char_matrix
 }
 
-fn paint_flat(img: &image::DynamicImage, cols: u32, rows: u32, invert: bool) -> Vec<char> {
+fn paint_flat(img: &image::DynamicImage, cols: u32, line_height: f32, invert: bool) -> Vec<char> {
     let tile_w = 10;
+    let tile_h = tile_w * line_height as u32;
     let w = cols * tile_w;
     let ar = img.width() as f32 / img.height() as f32;
+    let rows = (cols as f32 / (ar * line_height)) as u32;
     let h = (w as f32 / ar) as u32;
+    println!("cols {}, rows {}", cols, rows);
     println!("w: {}, h: {}", w, h);
 
     // TODO: don't make it bigger
@@ -93,8 +98,8 @@ fn paint_flat(img: &image::DynamicImage, cols: u32, rows: u32, invert: bool) -> 
     let chars = str.chars().collect::<Vec<char>>();
     let mut char_matrix = vec!['*'; (cols * rows) as usize];
     let scale = Scale {
-        x: tile_w as f32 * 2.0,
-        y: tile_w as f32 * 1.0,
+        x: tile_w as f32,
+        y: tile_h as f32,
     };
     let font = Vec::from(
         include_bytes!("../fonts/Caskaydia Cove Nerd Font Complete Regular.otf") as &[u8],
@@ -105,17 +110,9 @@ fn paint_flat(img: &image::DynamicImage, cols: u32, rows: u32, invert: bool) -> 
         .iter()
         .enumerate()
         .map(|(_, c)| {
-            let mut char_img = GrayImage::new(tile_w, tile_w);
+            let mut char_img = GrayImage::new(tile_w, tile_h);
             char_img.fill(255);
-            draw_text_mut(
-                &mut char_img,
-                Luma([0]),
-                0,
-                0,
-                scale,
-                &font,
-                &c.to_string(),
-            );
+            draw_text_mut(&mut char_img, Luma([0]), 0, 0, scale, &font, &c.to_string());
             // TODO: use cli option to enable this
             // char_img.save(&format!("out/{}.png", i)).unwrap();
             char_img
@@ -126,12 +123,12 @@ fn paint_flat(img: &image::DynamicImage, cols: u32, rows: u32, invert: bool) -> 
         let tile = img
             .crop_imm(
                 (i % cols) * tile_w,
-                (i as f32 / cols as f32).floor() as u32 * tile_w,
+                (i as f32 / cols as f32).floor() as u32 * tile_h,
                 tile_w,
-                tile_w,
+                tile_h,
             )
             .to_luma8();
-        if tile.width() != tile_w || tile.height() != tile_w {
+        if tile.width() != tile_w || tile.height() != tile_h {
             println!("tile size mismatch {} {}", tile.width(), tile.height());
             char_matrix[i as usize] = '_';
             continue;
@@ -164,15 +161,14 @@ fn paint_flat(img: &image::DynamicImage, cols: u32, rows: u32, invert: bool) -> 
 fn main() {
     let args = Args::parse();
     let img = image::open(&Path::new(&args.img)).unwrap();
-    let rows = (img.height() as f32 * args.cols as f32 / img.width() as f32) as u32;
+    let line_height = 2.0;
 
     println!("dimensions {:?}", img.dimensions());
     println!("color {:?}", img.color());
-    println!("cols {}, rows {}", args.cols, rows);
 
     let char_matrix = match args.mode {
-        Mode::Values => paint_values(&img, args.cols, rows, args.invert),
-        Mode::Flat => paint_flat(&img, args.cols, rows, args.invert),
+        Mode::Values => paint_values(&img, args.cols, line_height, args.invert),
+        Mode::Flat => paint_flat(&img, args.cols, line_height, args.invert),
     };
 
     for (i, c) in char_matrix.iter().enumerate() {
